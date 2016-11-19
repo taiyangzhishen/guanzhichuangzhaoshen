14c978a5abd4dc3200bdf9573b29962412c28ee5
taiyangzhishen <https://github.com/taiyangzhishen/guanzhichuangzhaoshen.git>
2016-11-19 09:09:40 +0800

    第四次作业case0

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace 第四次作业case0
+{
+    class Class1 : CollectionBase
+    {
+        public void Add(Class1 myClass1)
+        {
+            List.Add(myClass1);
+        }
+        public void Remove(Class1 myClass1)
+        {
+            List.Remove(myClass1);
+        }
+        public void Clear()
+        {
+            List.Clear();
+        }
+        public Class1 this[int Index]
+        {
+            get
+            {
+                return (Class1)List[Index];
+            }
+            set
+            {
+                List[Index] = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
